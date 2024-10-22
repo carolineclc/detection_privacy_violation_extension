2bab500c709029cc1dfb8c55bedcd82c6f381693
carolineclc <carolineclc@al.insper.edu.br>
2024-10-22 19:15:33 -0300

    adding third person cookies and first person cookie identifier

--- content_scripts/cookies_script.js
+++ content_scripts/cookies_script.js
@@ -5,8 +5,19 @@ function getActiveTab() {
     });
   }
 
+  function getDomain(url) {
+    try {
+      let domain = new URL(url).hostname;
+      return domain;
+    } catch (e) {
+      console.error('Invalid URL:', e);
+      return null;
+    }
+  }
+
 const showCookiesForTab = (tabs) => {
     let tab = tabs.pop();
+    let currentDomain = getDomain(tab.url);
     let countCookies = 0;
     var gettingAllCookies = browser.cookies.getAll({
       url: tab.url
@@ -16,16 +27,25 @@ const showCookiesForTab = (tabs) => {
     
       var activeTabUrl = document.getElementById('header-title-cookies');
       var text = document.createTextNode("Cookies at: "+tab.title);
-      var cookieList = document.getElementById('cookie-list');
+      var firstpartycookieList = document.getElementById('fp-cookie-list');
+      var thirdpartycookieList = document.getElementById('tp-cookie-list');
       var numberOfCookies = document.getElementById('number-cookies');
       activeTabUrl.appendChild(text);
   
       if (cookies.length > 0) {
         for (let cookie of cookies) {
-          let li = document.createElement("li");
-          let content = document.createTextNode(cookie.name + ": "+ cookie.value);
-          li.appendChild(content);
-          cookieList.appendChild(li);
+          if (cookie.domain === currentDomain || cookie.domain.endsWith(currentDomain)){
+            console.log("entrou");
+            let li = document.createElement("li");
+            let content = document.createTextNode(cookie.name + ": "+ cookie.value);
+            li.appendChild(content);
+            firstpartycookieList.appendChild(li);
+          } else{
+            let li = document.createElement("li");
+            let content = document.createTextNode(cookie.name + ": "+ cookie.value);
+            li.appendChild(content);
+            thirdpartycookieList.appendChild(li);
+          }
           countCookies++;
         }
         let cookiesText = document.createElement("p");
@@ -35,7 +55,7 @@ const showCookiesForTab = (tabs) => {
       } else {
         let p = document.createElement("p");
         let content = document.createTextNode("No cookies in this tab.");
-        let parent = cookieList.parentNode;
+        let parent = firstpartycookieList.parentNode;
   
         p.appendChild(content);
         parent.appendChild(p);
